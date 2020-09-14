9241e0afda3c8b946976ea4d5ac98303c50961dd
WuRoc <48498551+WuRoc@users.noreply.github.com>
2020-09-14 13:01:07 +0800

    Add files via upload

---
+++ Chapter_2/src/com/wuroc/chaptersix2/E15Protected.java
@@ -0,0 +1,17 @@
+//: E15Protected.java
+
+package com.wuroc.chaptersix2;
+
+/**
+ * @author WuRoc
+ * @GitHub www.github.com/WuRoc
+ * @version 1.0
+ * @2020年7月22日
+ * import static com.wuroc.util.Print.*;
+ * 
+ */
+public class E15Protected {
+	protected void f() {}
+
+}
+
